5f7877418797895a1e391bd7e3352833f5e7bf1d
KarmitP98 <kk07@my.yorku.ca>
2020-03-31 21:06:16 -0400

    Added frame variable

--- StartKit/mmu.c
+++ StartKit/mmu.c
@@ -3,7 +3,7 @@
 
 FILE *add_file_ptr;
 int i = 0;
-int log_add[1000], page_num[1000], offset[1000];
+unsigned int log_add[1000], page_num[1000], offset[1000], frame[10000];
 char temp[100];
 
 int main() {
@@ -14,6 +14,7 @@ int main() {
             page_num[i] = offset[i] = log_add[i] = atoi(temp);
             page_num[i] &= 65280;
             page_num[i] >>= 8;
+            frame[i] = page_num[i] / 256;
             offset[i] &= 255;
 
             printf("\n%d\t%d\t%d\t%d", i, log_add[i], page_num[i], offset[i]);
